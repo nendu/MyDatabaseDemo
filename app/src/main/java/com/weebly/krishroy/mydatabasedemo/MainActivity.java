d3f714e32773caf74cd23810876c7df0604aef68
Krishnendu Roy <krishroy@gmail.com>
2015-04-02 15:51:04 -0400

    Second Commit

--- app/src/main/java/com/weebly/krishroy/mydatabasedemo/MainActivity.java
+++ app/src/main/java/com/weebly/krishroy/mydatabasedemo/MainActivity.java
@@ -2,14 +2,24 @@ package com.weebly.krishroy.mydatabasedemo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText inputEditText;
+    TextView productsTextView;
+    myDBHandler mDBHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputEditText = (EditText) findViewById(R.id.inputField);
+
     }
 
 
